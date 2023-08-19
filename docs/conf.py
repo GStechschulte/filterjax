efdb1ab01ff126853a07324593fda7fb4cbc86ec
GStechschulte <stechschulteg@gmail.com>
2023-08-19 17:37:35 +0200

    update docs html title and sidebar contents

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# html_theme = 'alabaster'
 html_theme = 'sphinx_book_theme'
+html_title = "filterjax"
 html_theme_options = {
     'repository_url': 'https://github.com/GStechschulte/filterjax',
     "use_repository_button": True,
