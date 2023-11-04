6f4d0ca16b892d48c9a4a814703c374cba895a3d
GStechschulte <stechschulteg@gmail.com>
2023-11-04 12:21:28 +0100

    docstrings, check_dims method, and update docs

--- filterjax/inference/filters.py
+++ filterjax/inference/filters.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 import jax
 import jax.numpy as jnp
 
-from filterjax.params import KalmanParams
+from filterjax.models.params import KalmanParams
 
 
 class PosteriorFilter(NamedTuple):
@@ -14,42 +14,105 @@ class PosteriorFilter(NamedTuple):
     m : jnp.ndarray
         Mean of the posterior state estimate.
     P : jnp.ndarray
-        Covariance of the posterior state estimate.
+        Covariance matrix of the posterior state estimate.
     """
 
     mean: jnp.ndarray
     covariance: jnp.ndarray
 
 
-def update(y, x, R, H, P):
+def _update(y, m, R, H, P):
+    """Update (measurement) step conditions on a new linear Gaussian
+    observation and computes the Kalman gain to compute the posterior state mean
+    and covariance.
+
+    $$error := y - \mathbf{H}m$$
+    $$\mathbf{S} := \mathbf{H}\mathbf{P}\mathbf{H}^T + R$$
+    $$\mathbf{K} := \mathbf{P}\mathbf{H}^T\mathbf{S}^{-1}$$
+    $$m := m + \mathbf{K}y$$
+    $$\mathbf{P} := (\mathbf{I} - \mathbf{K}\mathbf{H})\mathbf{P}$$
+
+    Parameters
+    ----------
+    y : jnp.ndarray
+        Emissions (measurement) vector.
+    m : jnp.ndarray
+        Mean of the prior state estimate.
+    R : jnp.ndarray
+        Covariance matrix of the emission (observation) noise.
+    H : jnp.ndarray
+        Emission (observation) matrix.
+    P : jnp.ndarray
+        Covariance matrix of the prior state estimate.
+
+    Returns
+    -------
+    m : jnp.ndarray
+        Mean of the posterior state estimate.
+    P : jnp.ndarray
+        Covariance matrix of the posterior state estimate.
     """
-    Observe new measurement (emission) 'y' and update state.
-    """
-    error = y - (H @ x)
+    # compute residual between emission and prediction
+    error = y - (H @ m)
+
     # TODO: Use Cholesky decomposition for covariance
+    # compute Kalman gain (scaling factor)
     S = H @ P @ H.T + R
     K = P @ H.T @ jnp.linalg.inv(S)
-    x = x + K @ error
+
+    # update belief in state mean and covariance based on how certain we are
+    m = m + K @ error
     P = P - K @ S @ K.T
 
-    return x, P
+    return m, P
 
 
-def predict(F, Q, x, P, B=None, u=None):
+def _predict(F, Q, m, P, B=None, u=None):
+    """Predict next state mean and covariance using the Kalman filter state
+    propagation equations.
+
+    $$m := \mathbf{F}m + \mathbf{B}u$$
+    $$\mathbf{P} := \mathbf{F}\mathbf{P}\mathbf{F}^T + \mathbf{Q}$$
+
+    Parameters
+    ----------
+    F : jnp.ndarray
+        State transition matrix.
+    Q : jnp.ndarray
+        Covariance matrix of the process model (dynamics) noise.
+    m : jnp.ndarray
+        Mean of the prior state estimate.
+    P : jnp.ndarray
+        Covariance matrix of the prior state estimate.
+    B : jnp.ndarray, optional
+        Control transition matrix, by default None
+    u : jnp.ndarray, optional
+        Control vector, by default None
+
+    Returns
+    -------
+    m : jnp.ndarray
+        Mean of the posterior state estimate.
+    P : jnp.ndarray
+        Covariance matrix of the posterior state estimate.
     """
-    Predict next state using the Kalman filter state propagation equations.
-    """
+    # predict next state (prior) mean using the process model (state transition
+    # matrix) and control inputs if available
     if B is not None and u is not None:
-        x = F @ x + B @ u
+        m = F @ m + B @ u
     else:
-        x = F @ x
+        m = F @ m
 
+    # predict next state (prior) covariance using the process model (state
+    # transition matrix), state covariance, and process noise
     P = F @ P @ F.T + Q
 
-    return x, P
+    return m, P
 
 
-def batch_filter(params: KalmanParams, emissions: jnp.ndarray):
+def batch_filter(
+    params: KalmanParams, emissions: jnp.ndarray
+) -> PosteriorFilter:
 
     num_timesteps = len(emissions)
 
@@ -58,8 +121,8 @@ def batch_filter(params: KalmanParams, emissions: jnp.ndarray):
 
         # TODO: add and update log-likelihood
 
-        m, P = update(emissions[t], m, params.R, params.H, P)
-        m, P = predict(params.F, params.Q, m, P)
+        m, P = _update(emissions[t], m, params.R, params.H, P)
+        m, P = _predict(params.F, params.Q, m, P)
 
         return (m, P), (m, P)
 
--- filterjax/models/kalman_filter.py
+++ filterjax/models/kalman_filter.py
@@ -3,26 +3,18 @@ from typing import NamedTuple, Union
 import jax
 from jax import numpy as jnp
 
-from filterjax.params import KalmanParams
-from filterjax.inference.filters import batch_filter
-
-
-class PosteriorFilter(NamedTuple):
-    """Posterior state estimate and covariance matrix.
-
-    Parameters
-    ----------
-    m : jnp.ndarray
-        Mean of the posterior state estimate.
-    P : jnp.ndarray
-        Covariance of the posterior state estimate.
-    """
-
-    mean: jnp.ndarray
-    covariance: jnp.ndarray
+from filterjax.models.params import KalmanParams
+from filterjax.inference.filters import batch_filter, PosteriorFilter
 
 
 class KalmanFilter:
+    """Kalman filter for exact Bayesian filtering and smoothing.
+
+    The model is defined as follows:
+
+    # TODO: add model definition
+    """
+
     def __init__(self, state_dim, emission_dim):
         self.state_dim = state_dim
         self.emission_dim = emission_dim
@@ -37,8 +29,31 @@ class KalmanFilter:
         init_state_covariance: jnp.ndarray,
         init_control: Union[jnp.ndarray, None] = None,
     ) -> KalmanParams:
+        """Sets the initial parameters of the Kalman filter.
 
-        # TODO: add a method that checks dims of params before initializing
+        Parameters
+        ----------
+        init_state : jnp.ndarray
+            Initial state mean.
+        init_transition : jnp.ndarray
+            Initial state transition matrix.
+        init_emission : jnp.ndarray
+            Initial emission matrix.
+        init_emission_covariance : jnp.ndarray
+            Initial emission covariance matrix.
+        init_process_covariance : jnp.ndarray
+            Initial process covariance matrix.
+        init_state_covariance : jnp.ndarray
+            Initial state covariance matrix.
+        init_control : Union[jnp.ndarray, None], optional
+            Initial control matrix, by default None
+
+        Returns
+        -------
+        KalmanParams
+            Named tuple of Kalman parameters.
+        """
+
         params = KalmanParams(
             m=init_state,
             F=init_transition,
@@ -54,16 +69,72 @@ class KalmanFilter:
         return params
 
     def check_dims(self, params: KalmanParams):
-        assert params.m.shape == (self.state_dim,)
-        assert params.F.shape == (self.state_dim, self.state_dim)
-        assert params.H.shape == (self.emission_dim, self.state_dim)
-        assert params.R.shape == (self.emission_dim, self.emission_dim)
-        assert params.Q.shape == (self.state_dim, self.state_dim)
-        assert params.P.shape == (self.state_dim, self.state_dim)
+        """In order to perform matrix multiplication in inference, the
+        dimensions of the Kalman parameters must be checked with the state
+        and emission dimensions.
+
+        Parameters
+        ----------
+        params : KalmanParams
+            Kalman parameters.
+
+        Raises
+        ------
+        AssertionError
+            If the dimensions of the Kalman parameters do not match the state
+            and emission dimensions.
+        """
+        if params.m.shape != (self.state_dim,):
+            raise ValueError(
+                f"m.shape {params.m.shape} and {self.state_dim,} are incompatible."
+            )
+
+        if params.F.shape != (self.state_dim, self.state_dim):
+            raise ValueError(
+                f"F.shape {params.F.shape} and {self.state_dim, self.state_dim} are incompatible."
+            )
+
+        if params.H.shape != (self.emission_dim, self.state_dim):
+            raise ValueError(
+                f"H.shape {params.H.shape} and {self.emission_dim, self.state_dim} are incompatible."
+            )
+
+        if params.R.shape != (self.emission_dim, self.emission_dim):
+            raise ValueError(
+                f"R.shape {params.R.shape} and {self.emission_dim, self.emission_dim} are incompatible."
+            )
+
+        if params.Q.shape != (self.state_dim, self.state_dim):
+            raise ValueError(
+                f"Q.shape {params.Q.shape} and {self.state_dim, self.state_dim} are incompatible."
+            )
+
+        if params.P.shape != (self.state_dim, self.state_dim):
+            raise ValueError(
+                f"P.shape {params.P.shape} and {self.state_dim, self.state_dim} are incompatible."
+            )
 
         if params.B is not None:
-            assert params.B.shape == (self.state_dim, self.state_dim)
+            if params.B.shape != (self.state_dim, self.state_dim):
+                raise ValueError(
+                    f"B.shape {params.B.shape} and {self.state_dim, self.state_dim} are incompatible."
+                )
 
-    def filter(self, params: KalmanParams, emissions: jnp.ndarray):
+    def filter(
+        self, params: KalmanParams, emissions: jnp.ndarray
+    ) -> PosteriorFilter:
+        """Given a set (batch) emissions, performs online filtering.
+
+        Parameters
+        ----------
+        params : KalmanParams
+            Kalman parameters.
+        emissions : jnp.ndarray
+            Emissions (observations).
+
+        Returns
+        -------
+        PosteriorFilter
+            History (state) of the state mean and covariance.
+        """
         return batch_filter(params, emissions)
-
